b8db04877f0bd558a54ecfd4e75538334730a8c4
ladno338 <87599383+ladno338@users.noreply.github.com>
2024-01-18 14:30:37 +0200

    DontWorkingNameLoggout

--- shop/views.py
+++ shop/views.py
@@ -5,30 +5,24 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from shop.forms import BoardGameForm, AuthorCreationForm, AuthorUpdateForm
-from shop.models import Publisher, Author, BoardGame
+from shop.forms import (BoardGameForm,
+                        BoardgameSearchForm,
+                        AuthorCreationForm,
+                        AuthorUpdateForm)
+from shop.models import (Publisher,
+                         Author,
+                         BoardGame)
 
 
 # Create your views here.
 @login_required()
 def index(request):
     """View function for the home page of the site."""
-
-    # num_drivers = Driver.objects.count()
-    # num_cars = Car.objects.count()
-    # num_manufacturers = Manufacturer.objects.count()
-
     num_visits = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits + 1
     context = {
         "num_visits": num_visits + 1,
     }
-    # context = {
-    #     "num_drivers": num_drivers,
-    #     "num_cars": num_cars,
-    #     "num_manufacturers": num_manufacturers,
-    #     "num_visits": num_visits + 1,
-    # }
 
     return render(request, "shop/index.html", context=context)
 
@@ -91,6 +85,20 @@ class BoardgameListView(LoginRequiredMixin, generic.ListView):
     model = BoardGame
     context_object_name = "boardgame_list"
     template_name = "shop/boardgame_list.html"
+    queryset = BoardGame.objects.all().select_related("publisher")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(BoardgameListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = BoardgameSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        form = BoardgameSearchForm(self.request.GET)
+        if form.is_valid():
+            return self.queryset.filter(
+                name__icontains=form.cleaned_data["name"])
+        return self.queryset
 
 
 class BoardgameDetailView(LoginRequiredMixin, generic.DetailView):

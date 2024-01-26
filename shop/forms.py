8fd6472eb747f687de7f94f88b969101f67dbcba
ladno338 <87599383+ladno338@users.noreply.github.com>
2024-01-26 11:24:26 +0200

    Add files via upload

---
+++ shop/forms.py
@@ -0,0 +1,61 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from shop.models import BoardGame, Author
+
+
+class BoardGameForm(forms.ModelForm):
+    authors = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = BoardGame
+        fields = "__all__"
+
+
+class AuthorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Author
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+        )
+
+
+class AuthorUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = ["username"]
+
+
+class BoardgameSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}
+                               ),
+    )
+
+
+class AuthorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
+    )
+
+
+class PublisherSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+    )
+
